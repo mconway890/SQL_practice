b61707caa329d055393512934029b8d0c36498e9
Michele Conway <mconway890@gmail.com>
2019-05-31 11:56:15 -0400

    Insert book data into table

--- books.sql
+++ books.sql
@@ -5,3 +5,4 @@ INSERT INTO books (name, year, genre, pages, publisher, author_id) VALUES ("A Fe
 INSERT INTO books (name, year, genre, pages, publisher, author_id) VALUES ("The Two Towers", 1954, "Fantasy", 352, "Allen & Unwin", 3);
 INSERT INTO books (name, year, genre, pages, publisher, author_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, "Fantasy", 435, "Scholastic, Inc.", 2);
 INSERT INTO books (name, year, genre, pages, publisher, author_id) VALUES ("The Hitchhiker's Guide to the Galaxy", 1979, "Science Fiction", 224, "Pan Books", 4);
+INSERT INTO books (name, year, genre, pages, publisher, author_id) VALUES ("Ready Player One", 2011, "Science Fiction", 385, "Pan Books", 5);
